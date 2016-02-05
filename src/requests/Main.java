385f8862b3bfdda340241040caaca663c9cd6a0c
Git-Huy <chinook_@hotmail.fr>
2016-02-06 00:01:15 +0100

    lot of changes, testing

--- src/requests/Main.java
+++ src/requests/Main.java
@@ -1,6 +1,7 @@
 package requests;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,27 +11,59 @@ import java.sql.Statement;
 public class Main {
 	public static void main(String[] args) {
 
+//		String sql="";
+//		String url = "jdbc:mysql://sqletud.univ-mlv.fr/jchattou_db";
+//		String login="jchattou";
+//		String mdp="angels1707";
 		String sql="";
-		String url = "jdbc:mysql://sqletud.univ-mlv.fr/jchattou_db";
-		String login="jchattou";
-		String mdp="angels1707";
-		String id;
-		int nb_pattes;
+		String url = "jdbc:mysql://localhost:3306/gestion_projet?useSSL=false";
+		String login="root";
+		String mdp="mysql";
+		int id;
+		String adresse;
+		int num_dossier;
+		String date_debut;
+		String date_fin;
+		int personnel_id;
+		Date created;
 		
-//		sql = "SELECT * FROM Animal";
+		sql = "SELECT * from contact;";
+		Connection myCon = null;
 //
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver").newInstance();
-//			Connection myCon = DriverManager.getConnection(url, login, mdp);
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			myCon = DriverManager.getConnection(url, login, mdp);
 //			Statement st = myCon.createStatement();
 //			ResultSet set = st.executeQuery(sql);
 //			while (set.next()) {
-//				id = set.getString("id_espece");
-//				nb_pattes = set.getInt("nb_pattes");
-//				System.out.println(id + "," + nb_pattes);
+//				id = set.getInt("Contact_ID");
+//				adresse = set.getString("adresse");
+//				System.out.println("Voici l'id et l'adresse: " + id + "," + adresse);
 //			}
-//		} catch (ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException e) {
-//			e.printStackTrace();
-//		}
+//			ResultSet set = Requests.SELECT_Prescription(myCon, 1);
+//			set = Requests.SELECT_PATIENT( myCon, 1);
+//			while (set.next()) {
+//				id = set.getInt("prescription_id");
+//				num_dossier = set.getInt("num_dossier");
+//				date_debut = set.getString("date_debut");
+//				date_fin = set.getString("date_fin");
+//				personnel_id = set.getInt("personnel_id");
+//				System.out.println("result: " + id + " " + date_debut + " " + date_fin + " " + personnel_id);
+
+//			}
+
+			Requests.ADD_DMP_Prescription(myCon, 1,1, "mars", "avril", 1);
+			
+		} catch (ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		} finally {
+			if(myCon != null) {
+				try {
+					myCon.close();
+				} catch (SQLException ignore) {
+					
+				}
+			}
+		}
 	}
 }
--- src/requests/Requests.java
+++ src/requests/Requests.java
@@ -1,157 +1,247 @@
 package requests;
 
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Requests {
 	
-	//pour les requêtes imbriquées (si un jour il y a)
-	ResultSet savedRequest;
-	
 	//affiche les données d'un patient
-	public static void SELECT_PATIENT(Statement st, int id) throws SQLException {
-		String sql="SELECT* FROM Patients WHERE patient_id=" + "'" + id + "';";
-		System.out.println(sql);
-		ResultSet set = st.executeQuery(sql);
+	public static ResultSet SELECT_PATIENT(Connection conn, int id) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT* FROM Patients WHERE patient_id= (?)");
+		query.setInt(1,id);
+		ResultSet set = query.executeQuery();
+		return set;
 	}
 	
 	// ======================================
 	// =            Filtres de recherche    =
 	// ======================================
-	public static void SEARCH_DMP_BY_FILE_NUMBER(Statement st, int num_dossier) throws SQLException {
-		SELECT_Prescription(st, num_dossier);
-		SELECT_Elements(st, num_dossier);
-		SELECT_Episode(st, num_dossier);
-		SELECT_Antecedent(st, num_dossier);
-		SELECT_Doc(st, num_dossier);
+	public static List<ResultSet> SEARCH_DMP_BY_FILE_NUMBER(Connection conn, int num_dossier) throws SQLException {
+		ArrayList<ResultSet> result = new ArrayList<ResultSet>();
+		//les 5 parties forment le dossier
+		ResultSet presc = SELECT_Prescription(conn, num_dossier);
+		ResultSet elem = SELECT_Elements(conn, num_dossier);
+		ResultSet epi = SELECT_Episode(conn, num_dossier);
+		ResultSet antec = SELECT_Antecedent(conn, num_dossier);
+		ResultSet doc = SELECT_Doc(conn, num_dossier);
+		//ajout des résultats dans une liste
+		result.add(presc);
+		result.add(elem);
+		result.add(epi);
+		result.add(antec);
+		result.add(doc);
+		
+		return result;
 	}
 	
 	
 	//pas fini on doit stocker les valeurs des selects
-	public static void SEARCH_DMP_BY_FIRSTNAME(Statement st, String firstName) throws SQLException {
-		String sql="SELECT num_dossier FROM Patients WHERE prenom='" + firstName + "';";
-		ResultSet set = st.executeQuery(sql);
+	public static List<ResultSet> SEARCH_DMP_BY_FIRSTNAME(Connection conn, String firstName) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT num_dossier FROM Patients WHERE prenom=(?)");
+		query.setString(1, firstName);
+		ResultSet set = query.executeQuery();
 		int num_dossier;
+		ArrayList<ResultSet> result = new ArrayList<ResultSet>();
 		
 		while (set.next()) {
 			num_dossier = set.getInt("num_dossier");
-			SELECT_Prescription(st, num_dossier);
-			SELECT_Elements(st, num_dossier);
-			SELECT_Episode(st, num_dossier);
-			SELECT_Antecedent(st, num_dossier);
-			SELECT_Doc(st, num_dossier);
+			//les 5 parties forment le dossier
+			ResultSet presc = SELECT_Prescription(conn, num_dossier);
+			ResultSet elem = SELECT_Elements(conn, num_dossier);
+			ResultSet epi = SELECT_Episode(conn, num_dossier);
+			ResultSet antec = SELECT_Antecedent(conn, num_dossier);
+			ResultSet doc = SELECT_Doc(conn, num_dossier);
+			//les 5 premieres éléments correspondent au 1er numéro de dossier, les 5 autres aux 2ieme, etc...
+			result.add(presc);
+			result.add(elem);
+			result.add(epi);
+			result.add(antec);
+			result.add(doc);
 		}
+		return result;
 	}
 	
 	//pas fini on doit stocker les valeurs des selects
-	public static void SEARCH_DMP_BY_LASTNAME(Statement st, String lastName) throws SQLException {
-		String sql="SELECT num_dossier FROM Patients WHERE nom='" + lastName + "';";
-		ResultSet set = st.executeQuery(sql);
+	public static List<ResultSet> SEARCH_DMP_BY_LASTNAME(Connection conn, String lastName) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT num_dossier FROM Patients WHERE nom=(?)");
+		query.setString(1, lastName);
+		ResultSet set = query.executeQuery();
 		int num_dossier;
+		ArrayList<ResultSet> result = new ArrayList<ResultSet>();
 		
 		while (set.next()) {
 			num_dossier = set.getInt("num_dossier");
-			SELECT_Prescription(st, num_dossier);
-			SELECT_Elements(st, num_dossier);
-			SELECT_Episode(st, num_dossier);
-			SELECT_Antecedent(st, num_dossier);
-			SELECT_Doc(st, num_dossier);
+			//les 5 parties forment le dossier
+			ResultSet presc = SELECT_Prescription(conn, num_dossier);
+			ResultSet elem = SELECT_Elements(conn, num_dossier);
+			ResultSet epi = SELECT_Episode(conn, num_dossier);
+			ResultSet antec = SELECT_Antecedent(conn, num_dossier);
+			ResultSet doc = SELECT_Doc(conn, num_dossier);
+			//les 5 premieres éléments correspondent au 1er numéro de dossier, les 5 autres aux 2ieme, etc...
+			result.add(presc);
+			result.add(elem);
+			result.add(epi);
+			result.add(antec);
+			result.add(doc);
 		}
+		return result;
 	}
 	
-	public static void SEARCH_DMP_BY_PATIENT_ID(Statement st, int patient_id) throws SQLException {
+	public static List<ResultSet> SEARCH_DMP_BY_PATIENT_ID(Connection conn, int patient_id) throws SQLException {
 	
-		String sql="SELECT num_dossier FROM Patients WHERE patient_id='" + patient_id + "';";
-		ResultSet set = st.executeQuery(sql);
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT num_dossier FROM Patients WHERE patient_id=(?)");
+		query.setInt(1, patient_id);
+		ResultSet set = query.executeQuery();
 		int num_dossier;
+		ArrayList<ResultSet> result = new ArrayList<ResultSet>();
 		
 		//ne doit boucler qu'une fois en l'occurence car on ne possède qu'un numéro de dossier par patient
 		while (set.next()) {
 			num_dossier = set.getInt("num_dossier");
-			SELECT_Prescription(st, num_dossier);
-			SELECT_Elements(st, num_dossier);
-			SELECT_Episode(st, num_dossier);
-			SELECT_Antecedent(st, num_dossier);
-			SELECT_Doc(st, num_dossier);
+			//les 5 parties forment le dossier
+			ResultSet presc = SELECT_Prescription(conn, num_dossier);
+			ResultSet elem = SELECT_Elements(conn, num_dossier);
+			ResultSet epi = SELECT_Episode(conn, num_dossier);
+			ResultSet antec = SELECT_Antecedent(conn, num_dossier);
+			ResultSet doc = SELECT_Doc(conn, num_dossier);
+			result.add(presc);
+			result.add(elem);
+			result.add(epi);
+			result.add(antec);
+			result.add(doc);
 		}
 
+		return result;
 	}
 	
 	
 	// ======================================
-	// =            Dossier  Ajouts         =
+	// = Ajouts dans les parties du dossier =
 	// ======================================
 	
-	//affiche le DMP d'un patient par son ID , il faudra vérifier qu'il ne soit pas déja créé
-	public static void ADD_DMP(Statement st, int id) throws SQLException {
-		String sql="";
-		System.out.println(sql);
-		ResultSet set = st.executeQuery(sql);
+	//ajoute un dmp à un patient
+	public static void ADD_DMP(Connection conn, String libelle, String created, int patient_id) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Partie_Prescription VALUES (NULL,(?),(?),(?))");
+		query.setString(2,libelle);		
+		query.setString(3,created);
+		query.setInt(4,patient_id);
+		query.executeUpdate();
 	}
 	
 	// Ajoute à la partie Prescription du dossier les ID du DMP et de la prescription
-	public static void ADD_DMP_Prescription(Statement st, int DMP_ID, int PRESC_ID) throws SQLException {
-		String sql="INSERT INTO Partie_Prescription VALUES ('"+ DMP_ID + "','" + PRESC_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_DMP_Prescription(Connection conn, int PRESC_ID,int DMP_ID, String date_deb, String date_fin, int personnel_id) throws SQLException {
+		java.util.Date date = new java.util.Date();
+		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Partie_Prescription VALUES ((?),(?),(?),(?),(?),(?))");
+		query.setInt(1, PRESC_ID);
+		query.setInt(2, DMP_ID);
+		query.setString(3, date_deb);
+		query.setString(4, date_fin);
+		query.setInt(5, personnel_id);
+		query.setDate(6, sqlDate);
+		query.executeUpdate();
 	}
 	
-	public static void ADD_DMP_Episode(Statement st, int DMP_ID, int EPISODE_ID) throws SQLException {
-		String sql="INSERT INTO Partie_Prescription VALUES ('"+ DMP_ID + "','" + EPISODE_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_DMP_Episode(Connection conn, int EPISODE_ID, int DMP_ID, String date_debut, String date_derniere_visite, String notes, int personnel_id) throws SQLException {
+		java.util.Date date = new java.util.Date();
+		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Partie_Episode VALUES ((?),(?),(?),(?),(?),(?),(?))");
+		query.setInt(1, EPISODE_ID);
+		query.setInt(2, DMP_ID);
+		query.setString(3, date_debut);
+		query.setString(4, date_derniere_visite);
+		query.setString(5, notes);
+		query.setInt(6, personnel_id);
+		query.setDate(7, sqlDate);
+		query.executeUpdate();
 	}
 	
-	public static void ADD_DMP_Antecedent(Statement st, int DMP_ID, int ANTECEDENT_ID) throws SQLException {
-		String sql="INSERT INTO Partie_Prescription VALUES ('"+ DMP_ID + "','" + ANTECEDENT_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_DMP_Antecedent(Connection conn, int ANTECEDENT_ID, int DMP_ID, String date_debut, String date_fin, String notes, int personnel_id) throws SQLException {
+		java.util.Date date = new java.util.Date();
+		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Partie_Antecedent VALUES ((?),(?),(?),(?),(?),(?),(?)");
+		query.setInt(1, ANTECEDENT_ID);
+		query.setInt(2, DMP_ID);
+		query.setString(3, date_debut);
+		query.setString(4, date_fin);
+		query.setString(5, notes);
+		query.setInt(6, personnel_id);
+		query.setDate(7, sqlDate);
+		query.executeUpdate();
 	}
 	
-	public static void ADD_DMP_Elements(Statement st, int DMP_ID, int ELEMENTS_ID) throws SQLException {
-		String sql="INSERT INTO Partie_Prescription VALUES ('"+ DMP_ID + "','" + ELEMENTS_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_DMP_Elements(Connection conn, int ELEMENTS_ID, int DMP_ID, int personnel_id) throws SQLException {
+		java.util.Date date = new java.util.Date();
+		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Partie_Elements VALUES ((?),(?),(?),(?)");
+		query.setInt(1, ELEMENTS_ID);
+		query.setInt(2, DMP_ID);
+		query.setInt(3, personnel_id);
+		query.setDate(4, sqlDate);
+		query.executeUpdate();
 	}
 	
-	public static void ADD_DMP_Doc(Statement st, int DMP_ID, int DOC_ID) throws SQLException {
-		String sql="INSERT INTO Partie_Prescription VALUES ('"+ DMP_ID + "','" + DOC_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_DMP_Doc(Connection conn, int DOC_ID, int DMP_ID) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Partie_Elements VALUES ((?),(?))");
+		query.setInt(1, DOC_ID);
+		query.setInt(2, DMP_ID);
+		query.executeUpdate();
 	}
 	
 	// ======================================
 	// =            Dossier  SELECT         =
 	// ======================================
 		
-	public static void SELECT_Prescription(Statement st, int num_dossier) throws SQLException {
-		String sql="SELECT p.prescription_id, libelle, dosage, date_debut, date_fin, personnel_id FROM Partie_Prescription part, Prescriptions p WHERE num_dossier='" + num_dossier + "' AND part.prescription_id=p.prescription_id;";
-		ResultSet set = st.executeQuery(sql);
+	public static ResultSet SELECT_Prescription(Connection conn, int num_dossier) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT p.prescription_id, dosage, date_debut, date_fin, personnel_id FROM Partie_Prescription part, Prescriptions p WHERE num_dossier=(?) AND part.prescription_id=p.prescription_id");
+		query.setInt(1,num_dossier);
+		ResultSet set = query.executeQuery();
+		return set;
 	}
 	
-	public static void SELECT_Episode(Statement st, int num_dossier) throws SQLException {
-		String sql="SELECT ep.epOuvert_id, episode_libelle, date_debut, date_derniere_visite, notes, personnel_id FROM Partie_Episodes part, EpisodesEnCours ep WHERE num_dossier='" + num_dossier + "' AND part.epOuvert_id=ep.epOuvert_id;";
-		ResultSet set = st.executeQuery(sql);
+	public static ResultSet SELECT_Episode(Connection conn, int num_dossier) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT ep.epOuvert_id, episode_libelle, date_debut, date_derniere_visite, notes, personnel_id FROM Partie_Episodes part, EpisodesEnCours ep WHERE num_dossier=(?) AND part.epOuvert_id=ep.epOuvert_id");
+		query.setInt(1,num_dossier);
+		ResultSet set = query.executeQuery();
+		return set;
 	}
 	
-	public static void SELECT_Antecedent(Statement st, int num_dossier) throws SQLException {
-		String sql="SELECT ant.antecedent_id, antecedent_libelle, date_debut, date_fin, notes, personnel_id FROM Partie_Antecedents part, Antecedents ant WHERE num_dossier='" + num_dossier + "' AND part.antecedent_id=ant.antecedent_id;";
-		ResultSet set = st.executeQuery(sql);
+	public static ResultSet SELECT_Antecedent(Connection conn, int num_dossier) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT ant.antecedent_id, antecedent_libelle, date_debut, date_fin, notes, personnel_id FROM Partie_Antecedents part, Antecedents ant WHERE num_dossier=(?) AND part.antecedent_id=ant.antecedent_id");
+		query.setInt(1,num_dossier);
+		ResultSet set = query.executeQuery();
+		return set;
 	}
 	
-	public static void SELECT_Elements(Statement st, int num_dossier) throws SQLException {
-		String sql="SELECT e.ES_id, examen_type, examen_libelle, personnel_id FROM Partie_ElementsSuivis part, ElementsSuivis e WHERE num_dossier='" + num_dossier + "' AND part.ES_id=e.ES_id;";
-		ResultSet set = st.executeQuery(sql);
+	public static ResultSet SELECT_Elements(Connection conn, int num_dossier) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT e.ES_id, examen_type, examen_libelle, personnel_id FROM Partie_ElementsSuivis part, ElementsSuivis e WHERE num_dossier=(?) AND part.ES_id=e.ES_id;");
+		query.setInt(1,num_dossier);
+		ResultSet set = query.executeQuery();
+		return set;
 	}
 	
-	public static void SELECT_Doc(Statement st, int num_dossier) throws SQLException {
-		String sql="SELECT d.doc_id, contenu, size_file FROM Partie_DocumentsPatient part, Documents d WHERE num_dossier='" + num_dossier + "' AND part.doc_id=d.doc_id;";
-		ResultSet set = st.executeQuery(sql);
+	public static ResultSet SELECT_Doc(Connection conn, int num_dossier) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("SELECT d.doc_id, contenu, size_file FROM Partie_DocumentsPatient part, Documents d WHERE num_dossier=(?) AND part.doc_id=d.doc_id");
+		query.setInt(1,num_dossier);
+		ResultSet set = query.executeQuery();
+		return set;
 	}
 	
 	// ======================================
 	// =            Description             =
 	// ======================================
 
-	public static void ADD_Prescription(Statement st, int id, String libelle, String dosage, String date_deb, String date_fin, int personnel_ID, String created) throws SQLException {
-		String sql="INSERT INTO Prescriptions VALUES ('"+ id + "','" + libelle + "','" + dosage + "','" + date_deb + "','" + date_fin + "','" + personnel_ID + "','" + created + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_Prescription(Connection conn, int id, String libelle, String dosage) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Prescriptions VALUES ((?),(?),(?)");
+		query.setInt(1,id);
+		query.setString(2,libelle);
+		query.setString(2,dosage);
+		query.executeUpdate();
 	}
 		
 
@@ -159,35 +249,44 @@ public class Requests {
 	// =            Episode 	            =
 	// ======================================
 	
-	public static void ADD_Episode(Statement st, int id, String libelle, String date_deb, String date_derniere, String note, int personnel_ID, String created) throws SQLException {
-		String sql="INSERT INTO Prescriptions VALUES ('"+ id + "','" + libelle + "','" + date_deb + "','" + date_derniere + "','" + note + "','" + personnel_ID + "','" + created + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_Episode(Connection conn, int id, String libelle) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Prescriptions VALUES ((?),(?)");
+		query.setInt(1,id);
+		query.executeUpdate();
 	}
 		
 	// ======================================
 	// =            Antecedent              =
 	// ======================================
 	
-	public static void ADD_Antecedent(Statement st, int id, String libelle, String date_deb, String date_fin, String note, int personnel_ID, String created) throws SQLException {
-		String sql="INSERT INTO Prescriptions VALUES ('"+ id + "','" + libelle + "','" + date_deb + "','" + date_fin + "','" + note + "','" + personnel_ID + "','" + created + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_Antecedent(Connection conn, int id, String libelle) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Prescriptions VALUES ((?),(?)");
+		query.setInt(1,id);
+		query.setString(2,libelle);
+		query.executeUpdate();
 	}
 	
 	// ======================================
 	// =            Elements Suivis         =
 	// ======================================
 	
-	public static void ADD_Elements(Statement st, int id, String date_deb, String examen_type, String examen_libelle, int personnel_ID) throws SQLException {
-		String sql="INSERT INTO Prescriptions VALUES ('"+ id + "','" + date_deb + "','" + examen_type + "','" + examen_libelle + "','" + personnel_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_Elements(Connection conn, int id, String examen_type, String examen_libelle, float valeur1, String unite1, float valeur2, String unite2, String resultat_test) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Prescriptions VALUES ((?),(?),(?),(?)");
+		query.setInt(1,id);
+		query.executeUpdate();
 	}
 	
 	// ======================================
 	// =            Documents 	            =
 	// ======================================
-	public static void ADD_Doc(Statement st, int id, String contenu, float size_file, String created, int docType_ID) throws SQLException {
-		String sql="INSERT INTO Prescriptions VALUES ('"+ id + "','" + contenu + "','" + size_file + "','" + created + "','" + docType_ID + "');";
-		ResultSet set = st.executeQuery(sql);
+	public static void ADD_Doc(Connection conn, int id, Blob contenu, float size_file, String created, int docType_ID) throws SQLException {
+		PreparedStatement query = (PreparedStatement) conn.prepareStatement("INSERT INTO Prescriptions VALUES ((?),(?),(?),(?),(?)");
+		query.setInt(1,id);
+		query.setBlob(2,contenu);
+		query.setFloat(3,size_file);
+		query.setString(4,created);
+		query.setInt(5,docType_ID);
+		query.executeUpdate();
 	}
 		
 }
