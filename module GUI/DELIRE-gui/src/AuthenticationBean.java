b67520761b09594f41af3b211b46b431f65bc349
Jeremy bidet <jeremy.bidet@gmail.com>
2016-02-17 21:11:10 +0100

    add gui

---
+++ module GUI/DELIRE-gui/src/AuthenticationBean.java
@@ -0,0 +1,87 @@
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import com.sun.istack.internal.NotNull;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by Jeremy.
+ */
+
+@ManagedBean(name = "authenticate")
+@SessionScoped
+public class AuthenticationBean implements Serializable {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String rpps;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRpps() {
+        return this.rpps;
+    }
+
+    public void setRpps(String rpps) {
+        this.rpps = rpps;
+    }
+
+    public String login() {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        // TODO: CONVERGENCE: validate credentials from the database {username, password, rpps}
+        boolean loggedIn = AuthenticationDAO.validate(this.username, this.password, this.rpps);
+        if (loggedIn) {
+            HttpSession session = SessionBean.getSession();
+            session.setAttribute("username", this.username);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome" + this.username, null));
+            try {
+                context.getExternalContext().redirect("secure/medical_record.xhtml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "secure/medical_record";
+        } else {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error - Invalid credentials", null));
+            return "login";
+        }
+    }
+
+    public String logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpSession session = SessionBean.getSession();
+        session.invalidate();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Bye bye" + this.username, null));
+        try {
+            context.getExternalContext().redirect("login.xhtml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "login";
+    }
+
+}
