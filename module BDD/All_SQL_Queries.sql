7d1f65a462ccbb48650911aea95b53d4e77245fe
Jérémie Chattou <jj.chattou@gmail.com>
2016-02-25 18:48:42 +0100

    [Module BDD] password Sha1
    (30 min)
    - changement des mots de passe en Sha1 dans table User
    - changement champ mot de passe varchar(32) --> varchar(64)
    - génération des scripts.

--- module BDD/All_SQL_Queries.sql
+++ module BDD/All_SQL_Queries.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  sqletud.univ-mlv.fr
--- Généré le :  Sam 20 Février 2016 à 14:09
+-- Généré le :  Jeu 25 Février 2016 à 18:42
 -- Version du serveur :  5.5.40-0+wheezy1-log
 -- Version de PHP :  5.6.14-0+deb8u1
 
@@ -862,7 +862,7 @@ INSERT INTO `Specialites` (`specialite_id`, `specialite_libelle`) VALUES
 CREATE TABLE IF NOT EXISTS `Users` (
 `user_id` int(11) NOT NULL,
   `login` varchar(32) COLLATE latin1_general_ci NOT NULL,
-  `mot_passe` varchar(32) COLLATE latin1_general_ci NOT NULL,
+  `mot_passe` varchar(64) COLLATE latin1_general_ci NOT NULL,
   `created` datetime DEFAULT NULL,
   `personnel_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
@@ -872,26 +872,26 @@ CREATE TABLE IF NOT EXISTS `Users` (
 --
 
 INSERT INTO `Users` (`user_id`, `login`, `mot_passe`, `created`, `personnel_id`) VALUES
-(1, 'jmolina', 'passmolina', '2016-02-03 00:00:00', 1),
-(2, 'porcel', 'passorcel', '2016-02-03 00:00:00', 2),
-(3, 'fsimon', 'passsimon', '2016-02-03 00:00:00', 3),
-(4, 'fdesgrandchamps', 'passdesgrandchamps', '2016-02-03 00:00:00', 4),
-(5, 'edekerviler', 'passdekerviler', '2016-02-03 00:00:00', 5),
-(6, 'phenry', 'passhenry', '2016-02-03 00:00:00', 6),
-(7, 'pherman', 'passherman', '2016-02-03 00:00:00', 7),
-(8, 'hchabriat', 'passchabriat', '2016-02-03 00:00:00', 8),
-(9, 'rtadayoni', 'passtadayoni', '2016-02-03 00:00:00', 9),
-(10, 'acohensolal', 'passcohensolal', '2016-02-03 00:00:00', 10),
-(11, 'jbenifla', 'passbenifla', '2016-02-03 00:00:00', 11),
-(12, 'mallez', 'passallez', '2016-02-03 00:00:00', 12),
-(13, 'bmegarbane', 'passmegarbane', '2016-02-03 00:00:00', 13),
-(14, 'dpayendelagaranderie', 'passpayendelagaranderie', '2016-02-03 00:00:00', 14),
-(15, 'hbarreteau', 'passbarreteau', '2016-02-03 00:00:00', 15),
-(16, 'jlaredo', 'passlaredo', '2016-02-03 00:00:00', 16),
-(17, 'olaprevote', 'passlaprevote', '2016-02-03 00:00:00', 17),
-(18, 'salifa', 'passalifa', '2016-02-03 00:00:00', 18),
-(19, 'jbanas', 'passbanas', '2016-02-03 00:00:00', 19),
-(20, 'abonet', 'passbonet', '2016-02-03 00:00:00', 20);
+(1, 'jmolina', 'f618b1acd267cd661a134b8cc986ee43eda56a79 ', '2016-02-03 00:00:00', 1),
+(2, 'porcel', 'e3f6ddc0a9d6c3a96099268549ce3713eeaa47c6', '2016-02-03 00:00:00', 2),
+(3, 'fsimon', 'a8704b2753112510a06287051117708500b7680b', '2016-02-03 00:00:00', 3),
+(4, 'fdesgrandchamps', '1a03a70227d8a95680a16022f2ae8a14bdc4a8f8', '2016-02-03 00:00:00', 4),
+(5, 'edekerviler', '99373be6e4bc1fff8fd64d8e65662c4c7ddd59ee', '2016-02-03 00:00:00', 5),
+(6, 'phenry', '9acdcbf38da8780e9fd45183033ca36b8dace056', '2016-02-03 00:00:00', 6),
+(7, 'pherman', 'a94d404bc3f75cd6ae47721eb1f3e1ef3ce4a40e', '2016-02-03 00:00:00', 7),
+(8, 'hchabriat', '6891ea48977314cf8456b318312b8bbd5ddeb2a6', '2016-02-03 00:00:00', 8),
+(9, 'rtadayoni', '75ce65c47b25bc717cbc0be938ddedf88930134c', '2016-02-03 00:00:00', 9),
+(10, 'acohensolal', 'af20c23998c5e8c49701141a0c9d92fa3db88f82', '2016-02-03 00:00:00', 10),
+(11, 'jbenifla', '7f16544b5633dc092adc14d1c52f52c8ccf8a6dd', '2016-02-03 00:00:00', 11),
+(12, 'mallez', '752ac55da65d8c5574c67780588a4dd2c673b028', '2016-02-03 00:00:00', 12),
+(13, 'bmegarbane', 'bfaa06607861d62194bff967e93353e9e6776a97', '2016-02-03 00:00:00', 13),
+(14, 'dpayendelagaranderie', '34a5acb8f42542d5946b9b32ac6bf41603cf4672', '2016-02-03 00:00:00', 14),
+(15, 'hbarreteau', '8118ec9e26eeda7bf48dd2865a10b3c01d3291ee', '2016-02-03 00:00:00', 15),
+(16, 'jlaredo', '541a8d880d5887e28abf81fd8048773ae9c00dc4', '2016-02-03 00:00:00', 16),
+(17, 'olaprevote', 'fde4f9ea067461b68f939303951723919df08784', '2016-02-03 00:00:00', 17),
+(18, 'salifa', 'f8de051370bbf2030d6dfdeb3087b0e539357238', '2016-02-03 00:00:00', 18),
+(19, 'jbanas', '46a96d9e628ce25af39c4c42f7691cdd8d856049', '2016-02-03 00:00:00', 19),
+(20, 'abonet', 'e34dfde98930dabf89d7649ec48b5c2688456b64', '2016-02-03 00:00:00', 20);
 
 --
 -- Index pour les tables exportées
